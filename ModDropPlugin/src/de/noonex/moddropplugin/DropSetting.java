4f2d37726b2a8b6ae080a87a1d8f85c4cc8075c8
Denis Golubev <gdlog@t-online.de>
2011-06-28 13:44:03 +0200

    added the adt DropSetting

---
+++ ModDropPlugin/src/de/noonex/moddropplugin/DropSetting.java
@@ -0,0 +1,33 @@
+package de.noonex.moddropplugin;
+
+public class DropSetting 
+{
+	private AbstractDrop drop;
+	private int blockId;
+	
+	public DropSetting(AbstractDrop drop, int blockId)
+	{
+		this.drop = drop;
+		this.blockId = blockId;
+	}
+	
+	public AbstractDrop getDrop()
+	{
+		return drop;
+	}
+	
+	public void setDrop(AbstractDrop drop)
+	{
+		this.drop = drop;
+	}
+	
+	public int getBlockId()
+	{
+		return blockId;
+	}
+	
+	public void setBlockId(int blockId)
+	{
+		this.blockId = blockId;
+	}
+}
