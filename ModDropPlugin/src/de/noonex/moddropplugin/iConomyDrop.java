300affdd6c722452187436dd6ff073e018e1a389
Denis Golubev <gdlog@t-online.de>
2011-06-28 12:30:28 +0200

    added a new drop, it is possible to get money by destroying blocks now

---
+++ ModDropPlugin/src/de/noonex/moddropplugin/iConomyDrop.java
@@ -0,0 +1,40 @@
+package de.noonex.moddropplugin;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import com.iConomy.iConomy;
+import com.iConomy.system.Holdings;
+
+public class iConomyDrop extends AbstractDrop {
+	
+	double moneyamount;
+	
+	public iConomyDrop(double moneyamount)
+	{
+		this.moneyamount = moneyamount;
+	}
+
+	@Override
+	public void CreateDrop(Location loc, World world, Player player)
+	{
+		PluginHookManager pluginhook = PluginHookManager.getInstance();
+		
+		if(pluginhook.IsiConomyAvailable())
+		{
+			try {
+				iConomy icon = pluginhook.getiConomy();
+				Holdings playerBalance = icon.getAccount(player.getName()).getHoldings();
+				
+				playerBalance.add(moneyamount);
+			} catch (OperationNotSupportedException e) {
+				//do nothing
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
