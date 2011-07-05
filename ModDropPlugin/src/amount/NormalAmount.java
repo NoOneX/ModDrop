1eb3f69fa30effae60a8e396b78a38636f19d52e
Denis Golubev <gdlog@t-online.de>
2011-07-05 16:06:22 +0200

    added the basic functionality to support simple amounts

---
+++ ModDropPlugin/src/amount/NormalAmount.java
@@ -0,0 +1,36 @@
+package amount;
+
+import java.util.Random;
+
+
+public class NormalAmount extends Amount
+{
+	int probability;
+	int amount;
+	
+	public NormalAmount(int amount, int probability)
+	{
+		this.probability = probability;
+		this.amount = amount;
+	}
+
+	@Override
+	public void Do(IAmountable amountable)
+	{
+		if(amount == 0)
+		{
+			return;
+		}
+		
+		Random rnd = new Random();
+		if (rnd.nextInt(100) <= probability * 100)
+		{
+			for (int i = 0; i < amount; i++)
+			{
+				// or null as parameter?
+				amountable.Do(amount);
+			}
+		}
+	}
+
+}
