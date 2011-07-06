239c03993e7a0dc658d940851bd2c97fdcc0c009
Denis Golubev <gdlog@t-online.de>
2011-07-06 11:45:06 +0200

    added the new primed tnt drop, i still have to figure out how to set off the tnt

--- ModDropPlugin/src/de/noonex/moddropplugin/drops/DropStringParser.java
+++ ModDropPlugin/src/de/noonex/moddropplugin/drops/DropStringParser.java
@@ -15,50 +15,82 @@ public class DropStringParser
 	public static DropSetting ParseDrop(String str) throws ParseException
 	{
 		String[] arguments = str.split(":");
-		if(arguments.length != 4)
+		if (arguments.length != 4)
 		{
-			throw new ParseException("Bad format. Use this format: droptype:blockId:additionalarguments:amount", 0);
+			throw new ParseException(
+					"Bad format. Use this format: droptype:blockId:additionalarguments:amount",
+					0);
 		}
-		
-		if(arguments[0].equalsIgnoreCase( "block"))
+
+		if (arguments[0].equalsIgnoreCase("block"))
 		{
 			return parseBlockDrop(arguments);
 		}
-		else if(arguments[0].equalsIgnoreCase("monster"))
+		else if (arguments[0].equalsIgnoreCase("monster"))
 		{
 			return parseMonsterDrop(arguments);
 		}
-		else if(arguments[0].equalsIgnoreCase("icon"))
+		else if (arguments[0].equalsIgnoreCase("icon"))
 		{
 			return parseiConomyDrop(arguments);
 		}
+		else if (arguments[0].equalsIgnoreCase("primedtnt"))
+		{
+			return parsePrimedTNTDrop(arguments);
+		}
 		else
 		{
-			throw new ParseException("This keyword is not supported: " + arguments[0], 0);
+			throw new ParseException("This keyword is not supported: "
+					+ arguments[0], 0);
 		}
+	}
+
+	private static DropSetting parsePrimedTNTDrop(String[] arguments)
+			throws ParseException
+	{
+		AbstractDrop newDrop;
+		Amount amount;
+		int blockID;
+
+		try
+		{
+			blockID = Integer.parseInt(arguments[1]);
+		}
+		catch (NumberFormatException ex)
+		{
+			throw new ParseException(
+					"Bad format: BlockID or the amount are not numbers.", 0);
+		}
+		
+		amount = AmountParser.ParseAmount(arguments[3]);
+		newDrop = new PrimedTNTDrop();
+		newDrop.setAmount(amount);
+		
+		return new DropSetting(newDrop, blockID);
 	}
 
 	private static DropSetting parseiConomyDrop(String[] arguments)
 			throws ParseException
 	{
-		AbstractDrop newDrop;		
+		AbstractDrop newDrop;
 		Amount amount;
 		int blockID;
-		
+
 		try
-		{				
+		{
 			blockID = Integer.parseInt(arguments[1]);
 			amount = new iConomyAmount(Double.parseDouble(arguments[3]));
 		}
-		catch(NumberFormatException ex)
+		catch (NumberFormatException ex)
 		{
-			//TODO: Use the right offset
-			throw new ParseException("Bad format: BlockID or the amount are not numbers.", 0);
+			// TODO: Use the right offset
+			throw new ParseException(
+					"Bad format: BlockID or the amount are not numbers.", 0);
 		}
-		
+
 		newDrop = new iConomyDrop();
 		newDrop.setAmount(amount);
-		
+
 		return new DropSetting(newDrop, blockID);
 	}
 
@@ -66,27 +98,29 @@ public class DropStringParser
 			throws ParseException
 	{
 		AbstractDrop newDrop;
-		
+
 		Amount amount;
 		int blockID;
 		CreatureType creature;
-		
+
 		try
-		{				
+		{
 			blockID = Integer.parseInt(arguments[1]);
 			creature = CreatureType.fromName(arguments[2]);
 		}
-		catch(NumberFormatException ex)
+		catch (NumberFormatException ex)
 		{
-			//TODO: Use the right offset
-			throw new ParseException("Bad format: BlockID or the amount are not numbers. Or the creature name is wrong.", 0);
+			// TODO: Use the right offset
+			throw new ParseException(
+					"Bad format: BlockID or the amount are not numbers. Or the creature name is wrong.",
+					0);
 		}
-		
+
 		amount = AmountParser.ParseAmount(arguments[3]);
-		
+
 		newDrop = new MonsterDrop(creature);
 		newDrop.setAmount(amount);
-		
+
 		return new DropSetting(newDrop, blockID);
 	}
 
@@ -94,29 +128,31 @@ public class DropStringParser
 			throws ParseException
 	{
 		AbstractDrop newDrop;
-		
+
 		Amount amount;
 		int dropID;
 		int blockID;
 		Material material;
-		
+
 		try
-		{				
+		{
 			blockID = Integer.parseInt(arguments[1]);
 			dropID = Integer.parseInt(arguments[2]);
 		}
-		catch(NumberFormatException ex)
+		catch (NumberFormatException ex)
 		{
-			//TODO: Use the right offset
-			throw new ParseException("Bad format: BlockID, DropID or the amount are not numbers.", 0);
+			// TODO: Use the right offset
+			throw new ParseException(
+					"Bad format: BlockID, DropID or the amount are not numbers.",
+					0);
 		}
-		
+
 		amount = AmountParser.ParseAmount(arguments[3]);
-		
+
 		material = Material.getMaterial(dropID);
 		newDrop = new NormalDrop(material);
 		newDrop.setAmount(amount);
-		
+
 		return new DropSetting(newDrop, blockID);
 	}
 }
