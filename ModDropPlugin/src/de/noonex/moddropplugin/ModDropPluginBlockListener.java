53b4bb66a9b4742511b0bf478e5143a35238972e
Denis Golubev <gdlog@t-online.de>
2011-07-07 08:35:32 +0200

    improved conditions, if no condition apply, the normal drop is not cancelled.

--- ModDropPlugin/src/de/noonex/moddropplugin/ModDropPluginBlockListener.java
+++ ModDropPlugin/src/de/noonex/moddropplugin/ModDropPluginBlockListener.java
@@ -36,9 +36,11 @@ public class ModDropPluginBlockListener extends BlockListener
 				newDrop.CreateDrop(dropLocation, dropLocation.getWorld(), event.getPlayer(), true);
 				
 				//Cancel the normal drop
-				//ISSUE: You have to specify the normal drop if using world-condition
-				event.setCancelled(true);
-				event.getBlock().setType(Material.AIR);
+				if(!newDrop.getCancelled())
+				{
+					event.setCancelled(true);
+					event.getBlock().setType(Material.AIR);
+				}
 			}
 		}
 	}
