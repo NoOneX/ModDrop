83fb5668be62322f38bddadf38c0a96526976328
Denis Golubev <gdlog@t-online.de>
2011-07-25 18:07:54 +0200

    tool condition added, removed debug output

--- ModDropPlugin/src/de/noonex/moddropplugin/conditions/ConditionParser.java
+++ ModDropPlugin/src/de/noonex/moddropplugin/conditions/ConditionParser.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.bukkit.Material;
+
 public final class ConditionParser
 {
 	public static Condition[] ParseConditions(String conditionsString) throws ParseException
@@ -18,18 +20,28 @@ public final class ConditionParser
 			if(keyvalueArr[0].equalsIgnoreCase("world"))
 			{
 				conditions.add(ParseWorldCondition(keyvalueArr[1]));
-				System.out.println("world condition added: " + keyvalue);
 			}
 			else if(keyvalueArr[0].equalsIgnoreCase("damage"))
 			{
 				try
 				{
 					conditions.add(ParseDamageCondition(keyvalueArr[1]));
-					System.out.println("damage condition added: " + keyvalue);
 				}
 				catch(ParseException ex)
 				{
 					System.out.println("[ModDrop][WARNING] The damage condition value is not a number." + keyvalue);
+					continue;
+				}
+			}
+			else if(keyvalueArr[0].equalsIgnoreCase("tool"))
+			{
+				try
+				{
+					conditions.add(ParseToolCondition(keyvalueArr[1]));
+				}
+				catch(ParseException ex)
+				{
+					System.out.println("[ModDrop][WARNING] The tool condition value is not valid." + keyvalue);
 					continue;
 				}
 			}
@@ -45,6 +57,39 @@ public final class ConditionParser
 		}
 		
 		return conditions.toArray(new Condition[conditions.size()]);
+	}
+
+	private static Condition ParseToolCondition(String value) throws ParseException
+	{
+		Material blockInHand;
+		
+		blockInHand = Material.matchMaterial(value);
+		
+		if(blockInHand != null)
+		{
+			return new ToolCondition(blockInHand);
+		}
+		
+		//if block name isnt valid try blockid
+		
+		try
+		{
+			int toolID = Integer.parseInt(value);
+			blockInHand = Material.getMaterial(toolID);
+		}
+		catch (NumberFormatException ex)
+		{
+			throw new ParseException("The tool condition value is not valid", 0);
+		}
+		
+		
+		if(blockInHand != null)
+		{
+			return new ToolCondition(blockInHand);
+		}
+		
+		//if it still fails throw exception
+		throw new ParseException("The tool condition value is not valid", 0);
 	}
 
 	private static Condition ParseDamageCondition(String value) throws ParseException
