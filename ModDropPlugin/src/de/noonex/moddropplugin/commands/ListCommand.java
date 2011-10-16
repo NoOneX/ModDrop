72ca5d1f30d369604cbebfbbb2522ab5e8e85b30
Denis Golubev <gdlog@t-online.de>
2011-10-16 12:52:37 +0200

    Updated dependencies

--- ModDropPlugin/src/de/noonex/moddropplugin/commands/ListCommand.java
+++ ModDropPlugin/src/de/noonex/moddropplugin/commands/ListCommand.java
@@ -21,6 +21,7 @@ public class ListCommand extends AbstractCommand
 		
 		for(AbstractCommand command: commands.values())
 		{
+			System.out.println(String.format("[ModDrop: List] Command listed: %s", command.GetName()));
 			commandList += String.format("%s: %s%s", command.GetName(), command.GetDescription(), System.getProperty("line.separator"));
 		}
 		
